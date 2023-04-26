80cc3690225bb42316228815de061866aa3c180d
Arjun Sasi Kadukkappilly(UST <235808@ust-global.com>
2023-04-26 18:50:05 +0530

    add

--- Back_end/AdvInsurance/src/main/java/com/AdvInsurance/webservices/AdvInsurance/RestController/memberController.java
+++ Back_end/AdvInsurance/src/main/java/com/AdvInsurance/webservices/AdvInsurance/RestController/memberController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:58531")
 //@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
