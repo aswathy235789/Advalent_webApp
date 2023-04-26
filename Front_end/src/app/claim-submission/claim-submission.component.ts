f00c26a8a527606c91050ddb55da6d8b9571f5d7
Arjun Sasi Kadukkappilly(UST <235808@ust-global.com>
2023-04-26 14:51:41 +0530

    add

--- Front_end/src/app/claim-submission/claim-submission.component.ts
+++ Front_end/src/app/claim-submission/claim-submission.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,FormGroupDirective, AbstractControl, ValidatorFn, FormControl, FormArray  } from '@angular/forms';
 
 @Component({
   selector: 'app-claim-submission',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./claim-submission.component.css']
 })
 export class ClaimSubmissionComponent {
+  claimsubmission!: FormGroup;
+  constructor(private formBuilder: FormBuilder)
+   {
+
+   }
   
+
+  ngOnInit() {
+    this.claimsubmission = this.formBuilder.group({
+      providerName: ['', Validators.required],
+      providerType: ['', Validators.required],
+      address: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      typeOfClaim: ['', Validators.required],
+      dateOfService: ['', Validators.required],
+      cpt: ['', Validators.required],
+      icd: ['', Validators.required],
+      serviceReceived: ['', Validators.required],
+      amountCharged: ['', Validators.required]
+  
+
+
+    });
+
+
+  }
 }
 
 
