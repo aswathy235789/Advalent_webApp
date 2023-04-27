44c9e389f769fa4203b5c094a4db9fbf84d0ced8
Arjun Sasi Kadukkappilly(UST <235808@ust-global.com>
2023-04-27 13:00:59 +0530

    adjudicator added

--- Back_end/AdvInsurance/src/main/java/com/AdvInsurance/webservices/AdvInsurance/entity_classes/adjudicator.java
+++ Back_end/AdvInsurance/src/main/java/com/AdvInsurance/webservices/AdvInsurance/entity_classes/adjudicator.java
@@ -1,2 +1,29 @@
-package com.AdvInsurance.webservices.AdvInsurance.entity_classes;public class adjudicator {
+package com.AdvInsurance.webservices.AdvInsurance.entity_classes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "adjudicator")
+public class adjudicator {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
