a89c9aca87d93ddfe567bd1b02a9d224bfc2eb41
Rose Jose Vizhalil(UST <235799@ust-global.com>
2023-05-02 11:28:34 +0530

    backend

--- Back_end/AdvInsurance/src/main/java/com/AdvInsurance/webservices/AdvInsurance/repositories/Cpt_Icd_CodesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.AdvInsurance.webservices.AdvInsurance.repositories;
-
-import com.AdvInsurance.webservices.AdvInsurance.entity_classes.Cpt_Icd_Codes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Cpt_Icd_CodesRepository extends JpaRepository<Cpt_Icd_Codes, Long> {
-}
